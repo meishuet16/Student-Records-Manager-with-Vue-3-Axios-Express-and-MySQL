0de46fcc6d979ca88319cc64dac221a76afbe8b2
meishuet16 <leemeishuet@graduate.utm.my>
2026-05-13 12:04:57 +0800

    feat(ext-A): add server-side pagination with page and size query params

--- server/db.js
+++ server/db.js
@@ -1,4 +1,5 @@
 // server/db.js 
+require('dotenv').config()
 const mysql = require('mysql2/promise') 
   
 const pool = mysql.createPool({ 
@@ -11,3 +12,4 @@ const pool = mysql.createPool({
   connectionLimit: 10, 
   queueLimit: 0 
 }) 
+module.exports = pool
